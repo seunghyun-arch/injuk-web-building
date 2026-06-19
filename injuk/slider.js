feb4694a7a7deb9941a3f24a83960a67493139cf
seunghyun-arch <sophielee3667@gmail.com>
2026-06-19 20:11:02 +0900

    work.html, slider.js 설계 및 style.css에 work 관련 스타일 추가

---
+++ injuk/slider.js
@@ -0,0 +1,50 @@
+const track = document.getElementById('sliderTrack');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentIndex = 0;
+const totalSlides = slides.length;
+let autoPlayInterval;
+
+function moveToSlide(index) {
+    track.style.transform = 'translateX(-${index * 100}%)';
+    currentIndex = index;
+}
+
+function nextSlide() {
+    if (currentIndex === totalSlides - 1) {
+        moveToSlide(0);
+    } else {
+        moveToSlide(currentIndex + 1);
+    }
+}
+
+function prevSlide() {
+    if (currentIndex === 0) {
+        moveToSlide(totalSlides - 1);
+    } else {
+        moveToSlide(currentIndex - 1);
+    }
+}
+
+function startAutoPlay() {
+    autoPlayInterval = setInterval(nextSlide, 4000);
+}
+
+function resetAutoPlay() {
+    clearInterval(autoPlayInterval);
+    startAutoPlay();
+}
+
+nextBtn.addEventListener('click', () => {
+    nextSlide();
+    resetAutoPlay();
+});
+
+prevBtn.addEventListener('click', () => {
+    prevSlide();
+    resetAutoPlay();
+});
+
+startAutoPlay();
